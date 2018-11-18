77e8bcf9268630f20a5b7b2b35d9f2087e120ea5
Marcello Bastea-Forte <marcello@cellosoft.com>
2018-11-17 21:01:52 -0800

    add prettier, tweak stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx createElement */
+const { createElement } = require('react');
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import styles from './App.module.css';
+
+export function App() {
+  return (
+    <div className={styles.root}>
+      Hello!
+    </div>
+  );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
+
+/** @jsx createElement */
+const { createElement } = require('react');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
