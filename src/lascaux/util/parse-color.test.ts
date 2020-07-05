f74cce67febd8d2d57522c2f446fdbd17386c25b
Marcello Bastea-Forte <marcello@cellosoft.com>
2020-07-04 21:45:01 -0700

    fix tests

--- src/lascaux/util/parse-color.test.ts
+++ src/lascaux/util/parse-color.test.ts
@@ -2,9 +2,9 @@ import parseColor from './parse-color';
 
 describe('parseColor', () => {
   it('parses #ffffff', () => {
-    expect(parseColor('#ffffff')).toEqual([1, 1, 1]);
+    expect(parseColor('#ffffff')).toEqual([1, 1, 1, 1]);
   });
   it('parses #808080', () => {
-    expect(parseColor('#808080')).toEqual([128 / 255, 128 / 255, 128 / 255]);
+    expect(parseColor('#808080')).toEqual([128 / 255, 128 / 255, 128 / 255, 1]);
   });
 });
